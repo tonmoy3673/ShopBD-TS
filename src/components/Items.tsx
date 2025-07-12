2fbdd7705a77bc8a47a945f462c0073d893af56b
tonmoy <tonmoy3673@gmail.com>
2025-07-12 15:28:19 +0600

    :bulb: set currency format

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,4 +1,5 @@
-import { Card, CardBody, CardImg } from "react-bootstrap";
+import { Button, Card, CardBody, CardImg } from "react-bootstrap";
+import FormatCurrency from "../utilities/FormatCurrency";
 
 interface ItemData{
     id?: number,
@@ -10,11 +11,11 @@ interface ItemData{
 const Items = (item:ItemData) => {
 const {name,price,imgUrl} = item;
 
-
+const quantity:number =0;
 
 
     return (
-        <Card>
+        <Card className="h-100">
             {/* =========== Image ========== */}
             <CardImg 
             variant="top"
@@ -27,8 +28,15 @@ const {name,price,imgUrl} = item;
         <CardBody className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
             <span className="fs-5 ">{name}</span>
-             <span className="text-secondary fs-6">{price}</span>
+             <span className="text-secondary fs-6">{FormatCurrency(price)}</span>
         </Card.Title>
+
+        {/* ===================== Quantity ============ */}
+        <div className="mt-auto">
+        {
+            quantity===0? <Button className="w-100">+ Add to Cart</Button>:null
+        }
+        </div>
         </CardBody>
            
         </Card>
