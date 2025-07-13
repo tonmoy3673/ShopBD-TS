0da8003276863b807faa85aeedfd8deb2b872441
tonmoy <tonmoy3673@gmail.com>
2025-07-13 20:34:18 +0600

    :truck: set routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import Home from "./pages/Home";
-import About from "./pages/About";
 import Store from "./pages/Store";
 import RootLayout from "./layouts/RootLayout";
 
@@ -16,9 +14,7 @@ if (root) {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootLayout/>}>
-        <Route path="/" index element={<Home />} />
-        <Route path="/about" element={<About/>}/>
-        <Route path="/store" element={<Store/>}/>
+        <Route path="/" element={<Store/>}/>
         </Route>
         
       </Routes>
